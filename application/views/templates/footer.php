0b0ebad5297ed9fe71dd9245cbc96ee716fe25dc
webtek-hein <2151287@slu.edu.ph>
2018-01-22 21:19:04 +0800

    migrate to codeigniter

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,18 @@
-    </body>
+</body>
     <footer>
+        <!--   Core JS Files   -->
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
 
+        <!--  Checkbox, Radio & Switch Plugins -->
+        <script src="assets/js/bootstrap-checkbox-radio.js"></script>
+
+        <!--  Charts Plugin -->
+        <script src="assets/js/chartist.min.js"></script>
+
+        <!--  Notifications Plugin    -->
+        <script src="assets/js/bootstrap-notify.js"></script>
+
+        <!--  Data Table JS   -->
+        <script type="text/javascript" src="assets/js/jquery.dataTables.min.js"></script>
     </footer>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,141 @@
-<html>
-    <head>
-        <title>GSOIMS</title>
-    </head>
-    <body>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>GSO INVENTORY</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Paper Dashboard core CSS    -->
+    <link href="assets/css/paper-dashboard.css" rel="stylesheet"/>
+
+
+
+    <!--  Fonts and icons     -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/themify-icons.css" rel="stylesheet">
+
+    <link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/jquery.dataTables.min.css">
+
+    <!-- Bootstrap core CSS     -->
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+    <div class="sidebar" data-background-color="black" data-active-color="danger">
+
+        <!--
+            Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
+            Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
+        -->
+
+        <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="#" class="simple-text">
+                    GSO INVENTORY
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li>
+                    <a href="dashboard">
+                        <i class="fa fa-dashboard"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="user">
+                        <i class="ti-user"></i>
+                        <p>User Profile</p>
+                    </a>
+                </li>
+                <li  class="active">
+                    <a href="inventory">
+                        <i class="fa fa-bars"></i>
+                        <p>Inventory</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="department">
+                        <i class="fa fa-home"></i>
+                        <p>Department</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="return">
+                        <i class="fa fa-reply"></i>
+                        <p>Return</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="logs">
+                        <i class="ti-agenda"></i>
+                        <p>Logs</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-panel">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar bar1"></span>
+                        <span class="icon-bar bar2"></span>
+                        <span class="icon-bar bar3"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Table List</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="ti-panel"></i>
+                                <p>Stats</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="ti-bell"></i>
+                                <p class="notification">5</p>
+                                <p>Notifications</p>
+                                <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Notification 1</a></li>
+                                <li><a href="#">Notification 2</a></li>
+                                <li><a href="#">Notification 3</a></li>
+                                <li><a href="#">Notification 4</a></li>
+                                <li><a href="#">Another notification</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="ti-settings"></i>
+                                <p>Settings</p>
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
