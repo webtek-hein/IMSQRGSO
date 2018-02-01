f10ce18ced64b20b69c2948e2aa56d71d69501d8
Rusell Bayote <rusellbayote@gmail.com>
2018-02-01 14:40:38 +0800

    gsois db, added status for active and inactive

---
+++ exported database/version 4/gsois.sql
@@ -0,0 +1,436 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 01, 2018 at 06:38 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gsois`
+--
+CREATE DATABASE IF NOT EXISTS `gsois` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `gsois`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_code`
+--
+
+DROP TABLE IF EXISTS `account_code`;
+CREATE TABLE `account_code` (
+  `ac_id` int(3) NOT NULL,
+  `account_code` varchar(15) NOT NULL,
+  `description` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account_code`
+--
+
+INSERT INTO `account_code` (`ac_id`, `account_code`, `description`) VALUES
+(1, '1-07-01-010', '	Land'),
+(2, '1-07-02-010', 'Land Improvements, Aquaculture Structures'),
+(3, '1-07-03-050', '	Power Supply Systems'),
+(4, '1-07-04-010', '	Buildings'),
+(5, '1-07-04-020', '	School Buildings'),
+(6, '1-07-04-030', '	Hospitals and Health Centers'),
+(7, '1-07-04-990', '	Other Structures'),
+(8, '1-07-05-020', '	Office Equipment'),
+(9, '1-07-07-010', '	Furniture and Fixtures'),
+(10, '1-07-05-030', 'Information and Communication Technology Equi'),
+(12, '1-07-07-020', '	Books'),
+(13, '1-07-05-010', '	Machinery'),
+(14, '1-07-05-040', '	Agriculture and Forestry Equipment'),
+(15, '1-07-05-070', '	Communication Equipment'),
+(16, '1-07-05-080', '	Construction and Heavy Equipment'),
+(17, '1-07-05-090', '	Disaster Response and Rescue Equipment'),
+(18, '1-07-05-110', '	Medical Equipment'),
+(19, '1-07-05-100', 'Military, Police and Security Equipment'),
+(20, '1-07-05-130', '	Sports Equipment'),
+(21, '1-07-05-140', '	Technical and Scientific Equipment'),
+(22, '1-07-05-990', '	Other Machinery and Equipment'),
+(23, '1-07-06-010', '	Motor Vehicles'),
+(24, '1-07-03-010', '	Road Networks'),
+(25, '1-07-03-090', 'Parks, Plazas and Monuments'),
+(26, '1-07-03-040', '	Water Supply Systems'),
+(27, '1-07-03-020', '	Flood Control Systems'),
+(28, '1-07-03-990', '	Other Infrastructure Assets'),
+(29, '5-02-02-010', '	Training Expenses'),
+(30, '5-02-03-010', '	Office Supplies Expenses'),
+(31, '5-02-03-020', '	Accountable Forms Expenses'),
+(32, '5-02-03-040', '	Animal/Zoological Expenses'),
+(33, '5-02-03-050', '	Food Supplies Expenses'),
+(34, '5-02-03-070', '	Drugs and Medicines Expenses'),
+(35, '5-02-03-080', 'Medical, Dental and Laboratory Supplies Expen'),
+(36, '5-02-03-090', 'Fuel, Oil and Lubricants Expenses'),
+(37, '5-02-03-100', '	Agricultural and Marine Supplies Expenses'),
+(38, '5-02-03-110', '	Textbooks and Instructional Materials Expens'),
+(39, '5-02-03-120', 'Military, Police and Traffic Supplies Expense'),
+(40, '5-02-03-990', '	Other Supplies and Materials Expenses'),
+(41, '5-02-04-010', '	Water Expenses'),
+(42, '5-02-04-020', 'Electricity Expenses'),
+(43, '5-02-05-010', 'Postage and Courier Services'),
+(44, '5-02-05-020', 'Telephone Expenses'),
+(45, '5-02-05-030', 'Internet Subscription Expenses'),
+(46, '5-02-05-040', 'Cable, Satellite, Telegraph and Radio Expense'),
+(47, '5-02-99-060', 'Membership Dues and Contributions to Organiza'),
+(48, '5-02-06-010', '	Awards/Rewards Expenses'),
+(49, '5-02-99-010', '	Advertising Expenses'),
+(50, '5-02-99-020', '	Printing and Publication Expenses'),
+(51, '5-02-99-050', '	Rent Expenses'),
+(52, '5-02-99-030', '	Representation Expenses'),
+(53, '5-02-99-040', '	Transportation and Delivery Expenses'),
+(54, '5-02-99-070', '	Subscription Expenses'),
+(55, '5-02-07-010', '	Survey Expenses'),
+(56, '5-02-11-030', '	Consultancy Services'),
+(57, '5-02-12-010', '	Environment/Sanitary Services'),
+(58, '5-02-12-990', '	Other General Services'),
+(59, '5-02-12-020', '	Janitorial Services'),
+(60, '5-02-12-030', '	Security Services'),
+(61, '5-02-11-990', '	Other Professional Services'),
+(62, '5-02-13-020', '	Repairs and Maintenance - Land Improvements'),
+(63, '5-02-13-030', 'Repairs and Maintenance - Infrastructure Asse'),
+(64, '5-02-13-040', 'Repairs and Maintenance - Buildings and Other'),
+(65, '5-02-13-090', 'Repairs and Maintenance - Leased Assets Impro'),
+(66, '5-02-13-050', 'Repairs and Maintenance - Machinery and Equip'),
+(67, '5-02-13-070', 'Repairs and Maintenance - Furniture and Fixtu'),
+(68, '5-02-13-060', 'Repairs and Maintenance - Transportation Equi'),
+(69, '5-02-13-990', 'Repairs and Maintenance - Other Property, Pla'),
+(70, '5-02-14-020', '	Subsidy to NGAs'),
+(71, '5-02-14-030', '	Subsidy to Other Local Government Units'),
+(72, '5-02-99-080', '	Donations'),
+(73, '5-02-10-030', '	Extraordinary and Miscellaneous Expenses'),
+(74, '5-02-16-010', '	Taxes'),
+(75, '5-02-16-020', '	Fidelity Bond Premiums'),
+(76, '5-02-16-030', '	Insurance Expenses'),
+(77, '5-02-99-990', '	Other Maintenance and Operating Expenses'),
+(78, '5-03-01-040', '	Bank Charges'),
+(79, '1-07-04-040', '	Market'),
+(80, '1-07-04-050', '	Slaughterhouses'),
+(81, '5-02-03-060', '	Welfare Goods Expenses'),
+(85, '5-02-03-130', '	Chemical and Filtering Supplies Expenses');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `dept_id` int(4) NOT NULL,
+  `res_center_code` varchar(12) NOT NULL,
+  `department` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dept_id`, `res_center_code`, `department`) VALUES
+(11, '8731', 'CITY ENVIRONMENT & PARKS MANAGEMENT OFFICE'),
+(12, '1191', 'BUREAU OF FIRE PREVENTION AND SAFETY'),
+(13, '1061', 'GENERAL SERVICES OFFICE'),
+(14, '4411', 'HEALTH SERVICES OFFICE'),
+(15, '1011B', 'OFFICE OF THE CITY HUMAN RESOURCE CENTER'),
+(16, '1131', 'CITY LEGAL OFFICE'),
+(17, '1122', 'CITY LIBRARY'),
+(18, '1011', 'CITY MAYOR\'S OFFICE'),
+(19, '1158', 'MUNICIPAL TRIAL COURT IN CITIES'),
+(20, '1041', 'OFFICE OF THE CITY PLANNING AND DEVELOPMENT'),
+(21, '1181', 'CITY POLICE OFFICE'),
+(22, '9999F', 'PAROLE AND PROBATION OFFICE'),
+(23, '1141', 'CITY PROSECUTOR\'S OFFICE'),
+(24, '9999G', 'PUBLIC ATTORNEY\'S OFFICE'),
+(25, '1151', 'REGIONAL TRIAL COURT'),
+(26, '1161', 'REGISTRY OF DEEDS'),
+(27, '1021', 'SANGGUNIANG PANGLUNGSOD'),
+(28, '7611', 'OFFICE OF THE CITY SOCIAL WELFARE DEVELOPMENT'),
+(29, '1091', 'CITY TREASURER\'S OFFICE'),
+(30, '8721', 'CITY VETERINARY OFFICE'),
+(110, '8751', 'CITY ENGINEER\'S OFFICE'),
+(111, '1081', 'CITY ACCOUNTANT\'S OFFICE'),
+(112, '1031', 'CITY ADMINISTRATOR\'S OFFICE'),
+(113, '1101', 'CITY ASSESSOR\'S OFFICE'),
+(114, '1111', 'OFFICE OF THE CITY AUDITOR'),
+(115, '1071', 'CITY BUDGET OFFICE'),
+(116, '1101A', 'CITY BUILDING AND ARCHITECTURE OFFICE'),
+(117, '1171', 'CITY JAIL MANAGEMENT & PENOLOGY'),
+(118, '1051', 'OFFICE OF THE LOCAL CIVIL REGISTRAR'),
+(119, '3311', 'DEPARTMENT OF EDUCATION');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `distribution`
+--
+
+DROP TABLE IF EXISTS `distribution`;
+CREATE TABLE `distribution` (
+  `dist_id` int(15) NOT NULL,
+  `quantity_distributed` int(15) DEFAULT NULL,
+  `receivedby` varchar(45) DEFAULT NULL,
+  `dept_id` int(5) DEFAULT NULL,
+  `ac_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `distribution`
+--
+
+INSERT INTO `distribution` (`dist_id`, `quantity_distributed`, `receivedby`, `dept_id`, `ac_id`) VALUES
+(1, 1, 'test', 11, 1),
+(2, 1, 'test', 11, 1),
+(3, 1, 'test', 11, 1),
+(4, 5, 'test', 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `item_id` int(15) NOT NULL,
+  `quantity` int(9) DEFAULT NULL,
+  `item_name` varchar(60) NOT NULL,
+  `item_description` varchar(250) DEFAULT NULL,
+  `unit` varchar(20) DEFAULT NULL,
+  `item_type` enum('Capital Outlay','MOOE') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`item_id`, `quantity`, `item_name`, `item_description`, `unit`, `item_type`) VALUES
+(3, 0, 'Desktop', 'Intel Core 2 Duo ', 'piece', 'Capital Outlay');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemdetail`
+--
+
+DROP TABLE IF EXISTS `itemdetail`;
+CREATE TABLE `itemdetail` (
+  `item_det_id` int(15) NOT NULL,
+  `delivery_date` date DEFAULT NULL,
+  `date_received` date DEFAULT NULL,
+  `unit_cost` int(20) DEFAULT NULL,
+  `PO_no` int(15) DEFAULT NULL,
+  `PR_no` int(15) DEFAULT NULL,
+  `OBR_no` int(15) DEFAULT NULL,
+  `item_status` varchar(25) DEFAULT NULL,
+  `expiration_date` date DEFAULT NULL,
+  `item_id` int(15) DEFAULT NULL,
+  `dist_id` int(15) DEFAULT NULL,
+  `supplier_id` int(15) DEFAULT NULL,
+  `status` enum('Active','Inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `itemdetail`
+--
+DROP TRIGGER IF EXISTS `quantity`;
+DELIMITER $$
+CREATE TRIGGER `quantity` AFTER UPDATE ON `itemdetail` FOR EACH ROW BEGIN	
+   IF (new.dist_id is not null) THEN
+   	UPDATE item set quantity = quantity-1;
+    ELSE 
+    UPDATE item set quantity = quantity+1;
+    END IF;
+  END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `serial`
+--
+
+DROP TABLE IF EXISTS `serial`;
+CREATE TABLE `serial` (
+  `id` int(15) NOT NULL,
+  `serial` varchar(45) NOT NULL,
+  `item_det_id` int(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE `supplier` (
+  `supplier_id` int(15) NOT NULL,
+  `supplier_name` varchar(60) NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `location` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supplier_id`, `supplier_name`, `contact`, `location`) VALUES
+(1, 'test', '06065897', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `contact_no` varchar(15) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `dept_id` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account_code`
+--
+ALTER TABLE `account_code`
+  ADD PRIMARY KEY (`ac_id`),
+  ADD UNIQUE KEY `ac_id_UNIQUE` (`ac_id`),
+  ADD UNIQUE KEY `account_code_UNIQUE` (`account_code`),
+  ADD UNIQUE KEY `description_UNIQUE` (`description`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dept_id`),
+  ADD UNIQUE KEY `dept_id_UNIQUE` (`dept_id`),
+  ADD UNIQUE KEY `res_center_code_UNIQUE` (`res_center_code`),
+  ADD UNIQUE KEY `department_UNIQUE` (`department`);
+
+--
+-- Indexes for table `distribution`
+--
+ALTER TABLE `distribution`
+  ADD PRIMARY KEY (`dist_id`),
+  ADD KEY `department_idx` (`dept_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD UNIQUE KEY `item_id_UNIQUE` (`item_id`);
+
+--
+-- Indexes for table `itemdetail`
+--
+ALTER TABLE `itemdetail`
+  ADD PRIMARY KEY (`item_det_id`),
+  ADD UNIQUE KEY `item_det_id_UNIQUE` (`item_det_id`);
+
+--
+-- Indexes for table `serial`
+--
+ALTER TABLE `serial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supplier_id`),
+  ADD UNIQUE KEY `supplier_name_UNIQUE` (`supplier_name`),
+  ADD UNIQUE KEY `contact_UNIQUE` (`contact`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`),
+  ADD UNIQUE KEY `contact_no_UNIQUE` (`contact_no`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD KEY `department_idx` (`dept_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account_code`
+--
+ALTER TABLE `account_code`
+  MODIFY `ac_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `dept_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+--
+-- AUTO_INCREMENT for table `distribution`
+--
+ALTER TABLE `distribution`
+  MODIFY `dist_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `item`
+--
+ALTER TABLE `item`
+  MODIFY `item_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `itemdetail`
+--
+ALTER TABLE `itemdetail`
+  MODIFY `item_det_id` int(15) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `serial`
+--
+ALTER TABLE `serial`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `supplier_id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `department` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
