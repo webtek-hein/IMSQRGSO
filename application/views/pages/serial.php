ac7dbe940c380cb7f0995b7c7a2c30b4564960de
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-02-08 21:40:42 +0800

    update serial structure

--- application/views/pages/serial.php
+++ application/views/pages/serial.php
@@ -14,39 +14,143 @@
                         <h2>Serial</h2>
                         <div class="clearfix"></div>
                     </div>
-                    <!--Accordion-->
-                  <!-- add serial -->
-                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm">Add Serial</button>
-                  <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog modal-sm">
-                      <div class="modal-content">
+                <div class="accordion" id="accordion" role="tablist" aria-multiselectable="false">
+                    <!--ADD ITEM-->
+                    <div class="panel">
+                        <a class="panel-heading" role="tab" id="headingOne" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <h4 class="panel-title">Add Serial</h4>
+                        </a>
+                        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                            <div class="panel-body">
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <div class="x_panel">
+                                      <div class="x_content">
+                                        <div class="">
 
-                        <div class="modal-header">
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
-                          </button>
-                          <h4 class="modal-title" id="myModalLabel2">Add serial</h4>
-                        </div>
-                        <div class="modal-body">
-                            <form class="form-horizontal form-label-left input_mask" action="inventory/additem" method="POST">
-                                <div class="form-group">
-                                    <label>Serial no.</label>
-                                    <input type="text" name="serial" class="form-control" id="inputSuccess4" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label>Serial name</label>
-                                    <input type="text" name="serial" class="form-control" id="inputSuccess4" placeholder="">
-                                </div>
-                            </form>
-                          </div>
-                        <div class="modal-footer">
-                          <button class="btn btn-primary" type="button"><i class="fa fa-close" data-dismiss="modal"></i> Cancel</button>
-                          <button type="submit" class="btn btn-success"><i class="fa fa-send"></i> Submit</button>
-                        </div>
 
-                      </div>
-                    </div>
-                  </div>
-                <!-- end for add serial-->
+                                                <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                                                  <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
+                                                    <li role="presentation" class="active"><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">All Categories</a>
+                                                    </li>
+                                                    <li role="presentation" class=""><a href="#tab_content2" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false">Must Used</a>
+                                                    </li>
+                                                  </ul>
+                                                  <div id="myTabContent" class="tab-content">
+                                                    <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
+                                                    <!-- Start to do list -->
+                                                        <div class="col-md-12 col-sm-12 col-xs-12">
+                                                          <div class="x_panel">
+                                                            <div class="x_content">
+                                                              <div class="">
+                                                                <ul class="to_do">
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Laptop </p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> TV </p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Xerox Machine</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Water Tank</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Winshield</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> X-Ray Machine</p>
+                                                                  </li>
+                                                                </ul>
+                                                              </div>
+                                                            </div>
+                                                          </div>
+                                                        </div>
+                                                    <!-- End to do list -->
+                                                    </div>
+                                                    <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
+                                                    <!-- Start to do list -->
+                                                        <div class="col-md-12 col-sm-12 col-xs-12">
+                                                          <div class="x_panel">
+                                                            <div class="x_content">
+                                                              <div class="">
+                                                                <ul class="to_do">
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Laptop </p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Thermos</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Hammer</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Table Coffee</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Stool</p>
+                                                                  </li>
+                                                                  <li>
+                                                                    <p>
+                                                                      <input type="checkbox" class="flat"> Treadmill</p>
+                                                                  </li>
+                                                                </ul>
+                                                              </div>
+                                                            </div>
+                                                          </div>
+                                                        </div>
+                                                    <!-- End to do list -->
+                                                    </div>
+                                                    <!-- start to do list -->
+                                                    <div class="x_content">
+
+                                                        <form class="form-horizontal form-label-left" novalidate>
+                                                          <div class="item form-group">
+                                                            <label class="control-label col-md-3 col-sm-3 col-xs-7" for="name">Serial no. <span class="required"></span>
+                                                            </label>
+                                                            <div class="col-md-5 col-sm-7 col-xs-7">
+                                                              <input type="text" id="name" class="form-control col-md-7 col-xs-7" data-validate-length-range="30" data-validate-words="2" name="name" placeholder="serial" required="required">
+                                                            </div>
+                                                          </div>
+                                                          <div class="item form-group">
+                                                            <label class="control-label col-md-3 col-sm-3 col-xs-7" for="name">Item Name <span class="required"></span>
+                                                            </label>
+                                                            <div class="col-md-5 col-sm-7 col-xs-7">
+                                                              <input id="name" class="form-control col-md-7 col-xs-7" data-validate-length-range="30" data-validate-words="2" name="name" placeholder="Item name" required="required" type="text">
+                                                            </div>
+                                                          </div>
+                                                      </form>
+                                                      </div>
+                                                    <!-- End to do list -->
+                                                  </div>
+                                                    <div class="form-group">
+                                                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-4">
+                                                            <button class="btn btn-primary" type="button"><i class="fa fa-close"></i> Cancel</button>
+                                                            <button class="btn btn-danger" type="reset"><i class="fa fa-refresh"></i> Reset</button>
+                                                            <button type="submit" class="btn btn-success"><i class="fa fa-send"></i> Add Category</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+            <!--end of category-->
+
                 <div class="x_content">
                     <div class="x_panel">
                         <table id="datatable" data-pagination="true" data-search="true" data-toggle="table" data-url="inventory/serial" data-show-toggle="true" class="table table-striped table-bordered">
@@ -59,7 +163,11 @@
                             <tbody>
                             <tr>
                                 <td>71357</td>
-                                <td>71358</td>
+                                <td>TV</td>
+                            </tr>
+                            <tr>
+                                <td>98547</td>
+                                <td>Laptop</td>
                             </tr>
                             </tbody>
                             <tfoot>
