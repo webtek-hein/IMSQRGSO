885a6854eb6f202ca3d92eb8fd3254c4a70dc038
JoyCabildo <Joy_Cabildo24@yahoo.com>
2018-02-18 14:53:17 +0800

    Button Colors, removed unmatches  elements

--- application/views/pages/department.php
+++ application/views/pages/department.php
@@ -55,7 +55,6 @@
                                         </tr>
                                         <tbody>
                                         </tbody>
-                                        </thead>
                                     </table>
                                 </div>
                             </div>
