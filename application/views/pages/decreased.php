9a88ae2aa9c43f5633d896050fdd5b0547bfd850
Awh_Noh <markandawi@gmail.com>
2018-02-18 22:29:15 +0800

    minor_name fix

--- application/views/pages/decreased.php
+++ application/views/pages/decreased.php
@@ -11,7 +11,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>--Department's Name--</h2>
+                    <h2>Deacrease Logs</h2>
 
                     <div class="clearfix"></div>
                   </div>
--- application/views/pages/return_log.php
+++ application/views/pages/return_log.php
@@ -12,7 +12,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>--Department's Name--</h2>
+                    <h2>Return Log</h2>
 
                     <div class="clearfix"></div>
                   </div>
