6eeb30f3db33e9787de45570bc442cc2bb26b4b1
webtek-hein <2151287@slu.edu.ph>
2018-02-21 20:57:36 +0800

    fix html structure

--- application/views/pages/inventory.php
+++ application/views/pages/inventory.php
@@ -1,9 +1,9 @@
 <!-- page content -->
 <div id="inventory-main-page" class="page-content right_col" role="main" xmlns:height="http://www.w3.org/1999/xhtml">
     <div class="">
+        <div class="clearfix"></div>
         <!--inventory-->
         <div class="inventory-tab x_panel">
-            <div class="clearfix"></div>
             <!--Accordion-->
             <div class="accordion" id="accordion" role="tablist" aria-multiselectable="false">
                 <!--ADD Item-->
@@ -37,8 +37,8 @@
                                                     <form id="addItemForm" role="form" action="inventory/saveAll"
                                                           method="POST" data-parsley-validate="">
                                                         <div id="bulkdiv" class="tab-content">
-
-                                                            <div class="clone-tab tab-pane active" role="tabpanel"
+                                                        </div>
+                                                        <div class="clone-tab tab-pane active" role="tabpanel"
                                                                  id="step1B">
 
                                                                 <div class="left">
@@ -171,15 +171,13 @@
                                                     <button type="submit" id="saveALL" class="btn btn-default"><i
                                                                 class="fa fa-download"></i> Save All
                                                     </button>
-
-                                                    <div class="list-inline pull-right">
+                                                </div>
+                                                <div class="list-inline pull-right">
                                                         <button type="button" id="addanother"
                                                                 class="next-step btn btn-default"><i
                                                                     class="fa fa fa-cart-plus"></i> Add another item
                                                         </button>
                                                     </div>
-                                                </div>
-
                                             </div>
                                         </div>
                                     </div>
@@ -202,7 +200,6 @@
                                                                      data-toggle="tab" aria-expanded="false">MOOE</a>
                         </li>
                     </ul>
-
                     <div id="myTabContent" class="tab-content">
                         <!-- Capital Outaly tab-->
                         <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="CO-tab">
@@ -216,7 +213,6 @@
                                 </div>
                             </div>
                         </div>
-
                         <!--MOOE Tab-->
                         <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="MOOE-tab">
                             <!-- Implement Bootsrap table-->
@@ -246,81 +242,88 @@
                         </div>
                     </div>
                 </div>
-                <!-- end of Main Table Content-->
             </div>
-
-
+            <!-- end of Main Table Content-->
         </div>
-    </div>
-    <!-- detail tab -->
-    <div class="detail-tab x_panel hidden">
-        <button type="button" onclick="detail_back()" class="btn btn"></i> Back</a></button>
-        <div class="x_title" id="DetailsHead"><a id="changetoEdit" href="#"><i class="glyphicon glyphicon-edit"></i></a>
-            <form action="inventory/edititem" method="POST">
-                <h4>Item Name: <b id="itemname"></b></h4>
-                <p>Description: <b id="itemdesc"></b></p>
-                <p>Total Quantity: <b id="total"></b></p>
-                <p>Unit: <b id="unit"></b></p>
-                <p>Type: <b id="itemtype"></b></p>
-                <button type="submit" name="id" id="edtbutton" hidden>save</button>
-            </form>
-        </div>
-        <div class="clearfix"></div>
-        <!-- Main Table Content-->
-        <div role="tabpanel" class="tab-pane fade active in" id="tab_cont." aria-labelledby="CO-tab">
-            <a id="detailAddquantity" href="#" data-toggle="modal" data-target="#addquant"
+        <!-- detail tab -->
+        <div class="detail-tab x_panel hidden">
+            <button type="button" onclick="detail_back()" class="btn btn"></i> Back</a></button>
+            <div class="x_title" id="DetailsHead"><a id="changetoEdit" href="#"><i class="glyphicon glyphicon-edit"></i></a>
+                <form action="inventory/edititem" method="POST">
+                    <h4>Item Name: <b id="itemname"></b></h4>
+                    <p>Description: <b id="itemdesc"></b></p>
+                    <p>Total Quantity: <b id="total"></b></p>
+                    <p>Unit: <b id="unit"></b></p>
+                    <p>Type: <b id="itemtype"></b></p>
+                    <button type="submit" name="id" id="edtbutton" hidden>save</button>
+                </form>
+            </div>
+            <div class="clearfix"></div>
+            <!-- Main Table Content-->
+            <div role="tabpanel" class="tab-pane fade active in" id="tab_cont." aria-labelledby="CO-tab">
+                <a id="detailAddquantity" href="#" data-toggle="modal" data-target="#addquant"
                    class="btn btn-primary btn-xs"><i class="fa fa-plus-circle"></i> Add Quantity</a>
 
-            <!-- Add Quantity -->
-            <div id="addquant" class="modal fade Add_Item" tabindex="-1" role="dialog" aria-hidden="true">
+                <!-- Add Quantity -->
+                <div id="addquant" class="modal fade Add_Item" tabindex="-1" role="dialog" aria-hidden="true">
                     <div class="modal-dialog">
 
                         <!-- Modal content-->
                         <div class="modal-content">
                             <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                                <button type="button" class="close" data-dismiss="modal"><span
+                                            aria-hidden="true">×</span>
                                 </button>
                                 <h4 class="modal-title" id="myModalLabel">Add Quantity</h4>
                             </div>
                             <div class="modal-body">
-                                <form class="form-horizontal form-label-left" action="inventory/addquant" method="POST" novalidate>
+                                <form class="form-horizontal form-label-left" action="inventory/addquant" method="POST"
+                                      novalidate>
 
                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                                         <label>Quantity</label>
-                                        <input data-parsley-group="set2" data-parsley-trigger="blur" type="number" name="quant" min=0
+                                        <input data-parsley-group="set2" data-parsley-trigger="blur" type="number"
+                                               name="quant" min=0
                                                class="form-control has-feedback-left" placeholder="Quantity">
-                                        <span class="fa fa-plus-square-o form-control-feedback left" aria-hidden="true"></span>
+                                        <span class="fa fa-plus-square-o form-control-feedback left"
+                                              aria-hidden="true"></span>
                                     </div>
 
                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                                         <label>Unit Cost</label>
                                         <input type="number" min='0' name="cost" class="form-control has-feedback-left"
                                                id="inputSuccess3" placeholder="Unit Cost">
-                                        <span class="fa fa-circle-o form-control-feedback left" aria-hidden="true"></span>
+                                        <span class="fa fa-circle-o form-control-feedback left"
+                                              aria-hidden="true"></span>
                                     </div>
 
                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                                         <label>Delivery Date</label>
                                         <input type="date" name="del" class="form-control has-feedback-left">
-                                        <span class="fa fa-calendar-plus-o form-control-feedback left" aria-hidden="true"></span>
+                                        <span class="fa fa-calendar-plus-o form-control-feedback left"
+                                              aria-hidden="true"></span>
                                     </div>
 
                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                                         <label>Date Received</label>
                                         <input type="date" name="rec" data-validate-length-range="5,20"
                                                class="optional form-control has-feedback-left">
-                                        <span class="fa fa-calendar-check-o form-control-feedback left" aria-hidden="true"></span>
+                                        <span class="fa fa-calendar-check-o form-control-feedback left"
+                                              aria-hidden="true"></span>
                                     </div>
 
                                     <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
                                         <label>Expiration Date</label>
-                                        <input type="date" name="exp" data-validate-length-range="5,20" class="optional form-control has-feedback-left">
-                                        <span class="fa fa-calendar-times-o form-control-feedback left" aria-hidden="true"></span>
+                                        <input type="date" name="exp" data-validate-length-range="5,20"
+                                               class="optional form-control has-feedback-left">
+                                        <span class="fa fa-calendar-times-o form-control-feedback left"
+                                              aria-hidden="true"></span>
                                     </div>
 
                                     <div class="col-md-4 form-group has-feedback">
                                         <label>Supplier</label>
-                                        <select list="typelist" name="supp" class="supplieropt form-control has-feedback-left"
+                                        <select list="typelist" name="supp"
+                                                class="supplieropt form-control has-feedback-left"
                                                 placeholder="Type">
                                         </select>
                                         <span class="fa fa-truck form-control-feedback left" aria-hidden="true"></span>
@@ -330,12 +333,13 @@
                             </div>
 
                             <div class="modal-footer">
-                                    <button type="submit" class="btn-modal btn btn-default" id="savequant" name="id" value="1" id="quantsave">
-                                        <i class="fa fa-arrow-down">Save</i>
-                                    </button>
-                                    <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal"><i
-                                                class="fa fa-close">Cancel</i>
-                                    </button>
+                                <button type="submit" class="btn-modal btn btn-default" id="savequant" name="id"
+                                        value="1" id="quantsave">
+                                    <i class="fa fa-arrow-down">Save</i>
+                                </button>
+                                <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal"><i
+                                            class="fa fa-close">Cancel</i>
+                                </button>
                             </div>
 
                         </div>
@@ -343,10 +347,11 @@
                     </div>
                 </div>
 
-            <table id="detail-tab-table" class="table table-no-bordered table-hover">
-            </table>
-            <!-- Serial Accordion-->
-            <div id="data1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                <table id="detail-tab-table" class="table table-no-bordered table-hover">
+                </table>
+
+                <!-- Serial Accordion-->
+                <div id="data1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
                     <div class="panel-body">
                         <div class="col-md-offset-2">
                             <h4><b>List of Serial</b></h4>
@@ -361,28 +366,26 @@
                         </div>
                     </div>
                 </div>
-            <!-- /Serial Accordion-->
-        </div>
-        <!-- end of Main Table Content-->
-    </div>
+                <!-- /Serial Accordion-->
+            </div>
+            <!-- end of Main Table Content-->
+            <!--Distribution-->
+            <div class="modal fade Distribute" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog">
 
-    <!--Distribution-->
-    <div class="modal fade Distribute" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-                        </button>
-                        <h4 class="modal-title" id="myModalLabel">Distribution</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form role="form" class="form-horizontal form-label-left" action="inventory/distribute" method="POST" data-validate="parsley">
-                            <div class="tab-content">
-                                <div id="serial">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                            </button>
+                            <h4 class="modal-title" id="myModalLabel">Distribution</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form role="form" class="form-horizontal form-label-left" action="inventory/distribute" method="POST" data-validate="parsley">
+                                <div class="tab-content">
+                                    <div id="serial">
+                                    </div>
                                 </div>
-                            </div>
 
                                 <div class="col-md-6">
                                     <label for="name">Department</label>
@@ -417,17 +420,18 @@
                                         <input id="owner" class="form-control col-md-7 col-xs-12" name="owner"  type="text" placeholder="Supply Officer">
                                     </div>
                                 </div>
-                        </form>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" name="id" class="btn-modal btn btn-success" id="save1"><i class="fa fa-arrow-down"></i> Save</button>
+                            <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal">Cancel</button>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="submit" name="id" class="btn-modal btn btn-success" id="save1"><i class="fa fa-arrow-down"></i> Save</button>
-                        <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal">Cancel</button>
-                    </div>
+
                 </div>
-
             </div>
+            <!-- end of distribution-->
         </div>
-    <!-- end of distribution-->
-
+    </div>
 </div>
 
