14132d1be5fe51df2c2b646088efb813ec5219dd
Lyra Joyce Ronquillo <ljronki@gmail.com>
2018-03-11 15:18:56 +0800

    update dashboard

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -2,36 +2,91 @@
 <div class="page-content right_col" role="main">
     <!-- top tiles -->
     <div class="row tile_count">
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
-            <div class="count">2500</div>
-            <span class="count_bottom"><i class="green">4% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
-            <div class="count">123.50</div>
-            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
-            <div class="count green">2,500</div>
-            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
-            <div class="count">4,567</div>
+        <?php $position = $this->session->userdata['logged_in']['position'];
+        if ($position === 'Admin') {
+            echo '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-book" ></i > Total Items Received </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" > 4 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-clock-o" ></i > Expired Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="red" ><i class="fa fa-sort-desc" ></i > 3 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-undo" ></i > Returned Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" ><i class="fa fa-sort-asc" ></i > 34 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-truck"></i> Issued Items</span>
+            <div class="count">0</div>
             <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
-            <div class="count">2,315</div>
+        </div>' .
+                '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-money"></i> Total Cost</span>
+            <div class="count">0</div>
             <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-            <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
-            <div class="count">7,325</div>
+        </div>' .
+                '<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-users"></i> Total Users</span>
+            <div class="count">0</div>
             <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
-        </div>
+        </div>';
+        }
+
+        if($position === 'Custodian'){
+            echo '<div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-book" ></i > Total Items Received </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" > 4 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-clock-o" ></i > Expired Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="red" ><i class="fa fa-sort-desc" ></i > 3 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-undo" ></i > Returned Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" ><i class="fa fa-sort-asc" ></i > 34 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-truck"></i> Issued Items</span>
+            <div class="count">0</div>
+            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
+        </div>' .
+                '<div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-money"></i> Total Cost</span>
+            <div class="count">0</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+        </div>';
+        }
+
+        if($position == 'Supply Officer'){
+            echo '<div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-book" ></i > Total Items Received </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" > 4 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-clock-o" ></i > Expired Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="red" ><i class="fa fa-sort-desc" ></i > 3 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count" >
+            <span class="count_top" ><i class="fa fa-undo" ></i > Returned Items </span >
+            <div class="count" > 0</div >
+            <span class="count_bottom" ><i class="green" ><i class="fa fa-sort-asc" ></i > 34 % </i > From last Week </span >
+        </div >' .
+                '<div class="col-md-4 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-money"></i> Total Cost</span>
+            <div class="count">0</div>
+            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+        </div>';
+        }
+        ?>
     </div>
     <!-- /top tiles -->
 
@@ -56,7 +111,7 @@
                 </div>
                 <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
                     <div class="x_title">
-                        <h2>Top Campaign Performance</h2>
+                        <h4>Top Campaign Performance</h4>
                         <div class="clearfix"></div>
                     </div>
 
@@ -284,12 +339,6 @@
                                         </td>
                                         <td>15%</td>
                                     </tr>
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square red"></i>Others </p>
-                                        </td>
-                                        <td>30%</td>
-                                    </tr>
                                 </table>
                             </td>
                         </tr>
@@ -356,9 +405,9 @@
 
     <div class="row">
         <div class="col-md-4 col-sm-4 col-xs-12">
-            <div class="x_panel">
+            <div class="x_panel tile fixed_height_320">
                 <div class="x_title">
-                    <h2>Recent Activities <small>Sessions</small></h2>
+                    <h2>Recent Activities</h2>
                     <ul class="nav navbar-right panel_toolbox">
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                         </li>
@@ -394,48 +443,6 @@
                                     </div>
                                 </div>
                             </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for movie makers. They were where you met the producers that could fund your project, and if the buyers liked your flick, they’d pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for movie makers. They were where you met the producers that could fund your project, and if the buyers liked your flick, they’d pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="block">
-                                    <div class="block_content">
-                                        <h2 class="title">
-                                            <a>Who Needs Sundance When You’ve Got&nbsp;Crowdfunding?</a>
-                                        </h2>
-                                        <div class="byline">
-                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                        </div>
-                                        <p class="excerpt">Film festivals used to be do-or-die moments for movie makers. They were where you met the producers that could fund your project, and if the buyers liked your flick, they’d pay to Fast-forward and… <a>Read&nbsp;More</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </li>
                         </ul>
                     </div>
                 </div>
@@ -451,9 +458,9 @@
 
                 <!-- Start to do list -->
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                    <div class="x_panel">
+                    <div class="x_panel tile fixed_height_320">
                         <div class="x_title">
-                            <h2>To Do List <small>Sample tasks</small></h2>
+                            <h2>To Do List</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
@@ -499,18 +506,7 @@
                                         <p>
                                             <input type="checkbox" class="flat"> Food truck fixie locavors mcsweeney</p>
                                     </li>
-                                    <li>
-                                        <p>
-                                            <input type="checkbox" class="flat"> Create email address for new intern</p>
-                                    </li>
-                                    <li>
-                                        <p>
-                                            <input type="checkbox" class="flat"> Have IT fix the network printer</p>
-                                    </li>
-                                    <li>
-                                        <p>
-                                            <input type="checkbox" class="flat"> Copy backups to offsite location</p>
-                                    </li>
+
                                 </ul>
                             </div>
                         </div>
@@ -520,9 +516,9 @@
 
                 <!-- start of weather widget -->
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                    <div class="x_panel">
+                    <div class="x_panel tile fixed_height_320">
                         <div class="x_title">
-                            <h2>Daily active users <small>Sessions</small></h2>
+                            <h2>Daily active users</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
@@ -555,78 +551,17 @@
                                         <canvas height="84" width="84" id="partly-cloudy-day"></canvas>
                                     </div>
                                 </div>
-                                <div class="col-sm-8">
-                                    <div class="weather-text">
-                                        <h2>Texas <br><i>Partly Cloudy Day</i></h2>
-                                    </div>
-                                </div>
                             </div>
-                            <div class="col-sm-12">
-                                <div class="weather-text pull-right">
-                                    <h3 class="degrees">23</h3>
-                                </div>
-                            </div>
-
-                            <div class="clearfix"></div>
-
-                            <div class="row weather-days">
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Mon</h2>
-                                        <h3 class="degrees">25</h3>
-                                        <canvas id="clear-day" width="32" height="32"></canvas>
-                                        <h5>15 <i>km/h</i></h5>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Tue</h2>
-                                        <h3 class="degrees">25</h3>
-                                        <canvas height="32" width="32" id="rain"></canvas>
-                                        <h5>12 <i>km/h</i></h5>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Wed</h2>
-                                        <h3 class="degrees">27</h3>
-                                        <canvas height="32" width="32" id="snow"></canvas>
-                                        <h5>14 <i>km/h</i></h5>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Thu</h2>
-                                        <h3 class="degrees">28</h3>
-                                        <canvas height="32" width="32" id="sleet"></canvas>
-                                        <h5>15 <i>km/h</i></h5>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Fri</h2>
-                                        <h3 class="degrees">28</h3>
-                                        <canvas height="32" width="32" id="wind"></canvas>
-                                        <h5>11 <i>km/h</i></h5>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="daily-weather">
-                                        <h2 class="day">Sat</h2>
-                                        <h3 class="degrees">26</h3>
-                                        <canvas height="32" width="32" id="cloudy"></canvas>
-                                        <h5>10 <i>km/h</i></h5>
-                                    </div>
-                                </div>
                                 <div class="clearfix"></div>
                             </div>
                         </div>
                     </div>
-
                 </div>
+            <div class="clearfix"></div>
                 <!-- end of weather widget -->
             </div>
         </div>
     </div>
+<div class="clearfix"></div>
 </div>
 <!-- /page content -->
