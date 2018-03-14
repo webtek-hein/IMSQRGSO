90701b9db9e01de2e74b1f06092881fdefc1709a
webtek-hein <2151287@slu.edu.ph>
2018-03-14 23:30:24 +0800

    fix

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,7 +15,7 @@ if (isset($this->session->userdata['logged_in'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Inventory Management System</title>
-    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="description" content="Inventory Management System - GSO">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
