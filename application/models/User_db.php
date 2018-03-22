3f9c3a5b34a9793a8dd0a2ab64e08df9cd107a7b
Bacon_Pancake <markandawi@gmail.com>
2018-03-22 14:44:09 +0800

    can deactivate own account

--- application/models/User_db.php
+++ application/models/User_db.php
@@ -35,11 +35,9 @@ class User_db extends CI_Model {
 
     public function get_users()
     {
-         $user_id = $this->session->userdata['logged_in']['user_id'];
          $this->db->select('user_id,CONCAT(user.first_name," ", user.last_name) AS name,user.email,user.contact_no,
          user.username,user.position,dept.department,user.status');
         $this->db->join('gsois.department dept','dept.dept_id = user.dept_id','left');
-        $this->db->where('user_id !=', $user_id);     
         $query = $this->db->get('user');
         return $query->result_array();
     }
