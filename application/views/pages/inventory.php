d5e0b335f6b67d9c69fa5e5b1d05be59d0988f75
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2018-03-25 15:00:09 +0800

    remove title in modal

--- application/views/pages/inventory.php
+++ application/views/pages/inventory.php
@@ -882,7 +882,6 @@
                                                       <div class="modal-content">
                                                         <div class="modal-header">
                                                           <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                          <h4 class="modal-title">Generate QR</h4>
                                                         </div>
                                                         <div class="modal-body">
                                                           <p>generate qr</p>
