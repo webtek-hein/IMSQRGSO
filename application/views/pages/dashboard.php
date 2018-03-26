bcc62c7b8659b5817d477f0200d9bd40d9bf7c2b
Lyra Joyce Ronquillo <ljronki@gmail.com>
2018-03-26 23:24:55 +0800

    update dashboard

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -17,7 +17,9 @@
     </div>
 </div>
 
-    <div class="col-sm-6 col-lg-2">
+<?php $position = $this->session->userdata['logged_in']['position'];
+if ($position === 'Admin') {
+    echo '<div class="col-sm-6 col-lg-2">
         <div class="card1 text-white bg-flat-color-1" style="height: 130px; padding: 15px;">
             <div class="card1-body pb-0">
                 <i class="fa fa-book" style="font-size:40px;"></i>
@@ -26,75 +28,189 @@
                 </h4>
                 <p class="text-light">Total Items Received</p>
             </div>
-
         </div>
-    </div>
-    <!--/.col-->
+    </div>' .
 
-    <div class="col-sm-6 col-lg-2">
-        <div class="card1 text-white bg-flat-color-2" style="height: 130px; padding: 15px;">
+    '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-2" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-truck" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Issued Items </p >
+            </div >
+        </div >
+    </div >'.
+
+    '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-3" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-undo" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Returned Items </p >
+            </div >
+        </div >
+    </div >'.
+
+    '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-4" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-clock-o" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Expired Items </p >
+            </div >
+        </div >
+    </div >'.
+
+    '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-5" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-money" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Total Cost </p >
+            </div >
+        </div >
+    </div >'.
+
+    '<div class="col-sm-6 col-lg-2" >
+        <div class="dashnotif text-white bg-flat-color-6" style = "height: 130px; padding: 15px; background: #f49230;" >
+            <div class="car1d-body pb-0" >
+                <i class="fa fa-users" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Total Users </p >
+            </div >
+        </div >
+    </div >';
+    }
+    ?>
+
+<?php $position = $this->session->userdata['logged_in']['position'];
+if ($position === 'Custodian') {
+    echo '<div class="col-sm-8 col-lg-2">
+        <div class="card1 text-white bg-flat-color-1" style="height: 130px; padding: 15px;">
             <div class="card1-body pb-0">
-                <i class="fa fa-truck" style="font-size:40px;"></i>
+                <i class="fa fa-book" style="font-size:40px;"></i>
                 <h4 class="mb-0">
                     <span class="count">10468</span>
                 </h4>
-                <p class="text-light">Issued Items</p>
+                <p class="text-light">Total Items Received</p>
             </div>
         </div>
-    </div>
-    <!--/.col-->
+    </div>' .
 
-    <div class="col-sm-6 col-lg-2">
-        <div class="card1 text-white bg-flat-color-3" style="height: 130px; padding: 15px;">
+        '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-2" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-truck" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Issued Items </p >
+            </div >
+        </div >
+    </div >'.
+
+        '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-3" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-undo" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Returned Items </p >
+            </div >
+        </div >
+    </div >'.
+
+        '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-4" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-clock-o" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Expired Items </p >
+            </div >
+        </div >
+    </div >'.
+
+        '<div class="col-sm-6 col-lg-2" >
+        <div class="card1 text-white bg-flat-color-5" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-money" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Total Cost </p >
+            </div >
+        </div >
+    </div >';
+}
+?>
+
+<?php $position = $this->session->userdata['logged_in']['position'];
+if ($position === 'Supply Officer') {
+    echo '<div class="col-sm-6 col-lg-3">
+        <div class="card1 text-white bg-flat-color-1" style="height: 130px; padding: 15px;">
             <div class="card1-body pb-0">
-                <i class="fa fa-undo" style="font-size:40px;"></i>
+                <i class="fa fa-book" style="font-size:40px;"></i>
                 <h4 class="mb-0">
                     <span class="count">10468</span>
                 </h4>
-                <p class="text-light">Returned Items</p>
+                <p class="text-light">Total Items Received</p>
             </div>
         </div>
-    </div>
-    <!--/.col-->
+    </div>' .
 
-    <div class="col-sm-6 col-lg-2">
-        <div class="card1 text-white bg-flat-color-4" style="height: 130px; padding: 15px;">
-            <div class="card1-body pb-0">
-                <i class="fa fa-clock-o" style="font-size:40px;"></i>
-                <h4 class="mb-0">
-                    <span class="count">10468</span>
-                </h4>
-                <p class="text-light">Expired Items</p>
-            </div>
-        </div>
-    </div>
-    <!--/.col-->
+        '<div class="col-sm-6 col-lg-3" >
+        <div class="card1 text-white bg-flat-color-3" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-undo" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Returned Items </p >
+            </div >
+        </div >
+    </div >'.
 
-    <div class="col-sm-6 col-lg-2">
-        <div class="card1 text-white bg-flat-color-5" style="height: 130px; padding: 15px;">
-            <div class="card1-body pb-0">
-                <i class="fa fa-money" style="font-size:40px;"></i>
-                <h4 class="mb-0">
-                    <span class="count">10468</span>
-                </h4>
-                <p class="text-light">Total Cost</p>
-            </div>
-        </div>
-    </div>
-    <!--/.col-->
+        '<div class="col-sm-6 col-lg-3" >
+        <div class="card1 text-white bg-flat-color-4" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-clock-o" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Expired Items </p >
+            </div >
+        </div >
+    </div >'.
 
-    <div class="col-sm-6 col-lg-2">
-        <div class="dashnotif text-white bg-flat-color-6" style="height: 130px; padding: 15px; background: #f49230;">
-            <div class="car1d-body pb-0">
-                <i class="fa fa-users" style="font-size:40px;"></i>
-                <h4 class="mb-0">
-                    <span class="count">10468</span>
-                </h4>
-                <p class="text-light">Total Users</p>
-            </div>
-        </div>
-    </div>
-    <!--/.col-->
+        '<div class="col-sm-6 col-lg-3" >
+        <div class="card1 text-white bg-flat-color-5" style = "height: 130px; padding: 15px;" >
+            <div class="card1-body pb-0" >
+                <i class="fa fa-money" style = "font-size:40px;" ></i >
+                <h4 class="mb-0" >
+                    <span class="count" > 10468</span >
+                </h4 >
+                <p class="text-light" > Total Cost </p >
+            </div >
+        </div >
+    </div >';
+
+}
+?>
+
+
 
     <div class="col-xl-12">
         <div class="card" style="margin-top: 20px;">
