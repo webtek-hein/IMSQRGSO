d9def0208a2a9e9fbadda5e70ee1e90d71a503e6
gloridhel <gloridhelgoyo@yahoo.com>
2018-05-20 10:06:15 +0800

    updated Dashboard...

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -154,60 +154,73 @@ if ($position === 'Custodian') {
 
 <?php $position = $this->session->userdata['logged_in']['position'];
 if ($position === 'Supply Officer') {
-    echo '<div class="col-sm-6 col-lg-3">
-        <div class="card1 text-white bg-flat-color-1" style="height: 130px; padding: 15px;">
-            <div class="card1-body pb-0">
-                <i class="fa fa-book" style="font-size:40px;"></i>
-                <h4 class="mb-0" id="itemsrec">
-                </h4>
-                <p class="text-light">New Items Received</p>
+    echo '<div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-primary o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-comments"></i>
+              </div>
+              <div class="mr-5" id="itemsrec">New Item Received</div>
             </div>
-        </div>
-    </div>'.
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>'.
 
-        '<div class="col-sm-6 col-lg-3" >
-        <div class="card1 text-white bg-flat-color-2" style = "height: 130px; padding: 15px;" >
-            <div class="card1-body pb-0" >
-                <i class="fa fa-truck" style = "font-size:40px;" ></i >
-                <h4 class="mb-0" id="itemsiss">
-                </h4 >
-                <p class="text-light" > Issued Items </p >
-            </div >
-        </div >
-    </div >'.
+        '<div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-warning o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-list"></i>
+              </div>
+              <div class="mr-5" id="itemsiss">Issued Item</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>'.
 
-        '<div class="col-sm-6 col-lg-3" >
-        <div class="card1 text-white bg-flat-color-3" style = "height: 130px; padding: 15px;" >
-            <div class="card1-body pb-0" >
-                <i class="fa fa-undo" style = "font-size:40px;" ></i >
-                <h4 class="mb-0" id="retitem">
-                </h4 >
-                <p class="text-light" > Returned Items </p >
-            </div >
-        </div >
-    </div >'.
+        '<div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-success o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-shopping-cart"></i>
+              </div>
+              <div class="mr-5" id="retitem">Returned Items</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>'.
 
-        '<div class="col-sm-6 col-lg-3" >
-        <div class="card1 text-white bg-flat-color-4" style = "height: 130px; padding: 15px;" >
-            <div class="card1-body pb-0" >
-                <i class="fa fa-clock-o" style = "font-size:40px;" ></i >
-                <h4 class="mb-0" id="expitems">
-                </h4 >
-                <p class="text-light" > Expired Items </p >
-            </div >
-        </div >
-    </div >'.
-
-        '<div class="col-sm-6 col-lg-3" >
-        <div class="card1 text-white bg-flat-color-5" style = "height: 130px; padding: 15px;" >
-            <div class="card1-body pb-0" >
-                <i class="fa fa-money" style = "font-size:40px;" ></i >
-                <h4 class="mb-0" id="tcost">
-                </h4 >
-                <p class="text-light" > Total Inventory Cost </p >
-            </div >
-        </div >
-    </div >';
+        '<div class="col-xl-3 col-sm-6 mb-3">
+          <div class="card text-white bg-danger o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-support"></i>
+              </div>
+              <div class="mr-5" id="expitems">Expired Item</div>
+            </div>
+            <a class="card-footer text-white clearfix small z-1" href="#">
+              <span class="float-left">View Details</span>
+              <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+              </span>
+            </a>
+          </div>
+        </div>';
 }
 ?>
 
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -36,6 +36,8 @@
 }
 ?>
 <script src="assets/js/lib/chart-js/Chart.min.js"></script>
+<script src="assets/js/vendor/jquery/jquery.dataTables.js"></script>
+<script src="assets/js/vendor/jquery/jquery.min.js"></script>
 <script src="assets/js/lib/vector-map/jquery.vmap.min.js"></script>
 <script src="assets/js/lib/vector-map/jquery.vmap.sampledata.js"></script>
 <script src="assets/js/lib/vector-map/country/jquery.vmap.world.js"></script>
